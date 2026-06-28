07758c88defc8d0ad6942544a595b731ef4cc34d
Anish Neupane <anishneupane1@gmail.com>
2026-06-28 00:37:52 -0700

    Refresh

--- internal/config/limiter.go
+++ internal/config/limiter.go
@@ -20,161 +20,47 @@ type RateLimiter struct {
 	AccountVerificationUserID valkeylimiter.RateLimiterClient
 	ResendVerification        valkeylimiter.RateLimiterClient
 	ResendVerificationUserID  valkeylimiter.RateLimiterClient
+	Refresh                   valkeylimiter.RateLimiterClient
+	RefreshUserID             valkeylimiter.RateLimiterClient
+}
+
+type limiterTask struct {
+	target *valkeylimiter.RateLimiterClient
+	prefix string
+	limit  int
+	window time.Duration
 }
 
 func NewRateLimiter(client valkey.Client) (*RateLimiter, error) {
 	limiter := &RateLimiter{}
 
-	if sessionErr := initSessionLimiter(limiter, client); sessionErr != nil {
-		return nil, sessionErr
+	tasks := []limiterTask{
+		{&limiter.Login, loginLimiterSessionPrefix, limiterLimit, limiterWindowSession},
+		{&limiter.LoginTwoFactor, loginTwoFactorSessionLimiterPrefix, limiterLimit, limiterWindowSession},
+		{&limiter.ForgetPassword, fpLimiterPrefix, limiterLimit, limiterWindowSession},
+		{&limiter.Verification, verificationSessionLimiterPrefix, limiterLimit, limiterWindowSession},
+		{&limiter.ResetPassword, rpSessionLimiterPrefix, limiterLimit, limiterWindowSession},
+		{&limiter.ResendVerification, resendVerificationSessionLimiterPrefix, limiterLimit, limiterWindowSession},
+		{&limiter.AccountVerification, accountVerificationSessionLimiterPrefix, limiterLimit, limiterWindowSession},
+		{&limiter.Refresh, refreshSessionLimiterPrefix, refreshSessionLimiterLimit, limiterRefreshWindowSession},
+
+		{&limiter.LoginTwoFactorUserID, loginTwoFactorLimiterPrefix, limiterLimit, limiterWindowUserID},
+		{&limiter.VerificationUserID, verificationLimiterPrefix, limiterLimit, limiterWindowUserID},
+		{&limiter.ResetPasswordUserID, rpLimiterPrefix, limiterLimit, limiterWindowUserID},
+		{&limiter.ResendVerificationUserID, resendVerificationLimiterPrefix, limiterLimit, limiterWindowUserID},
+		{&limiter.AccountVerificationUserID, accountVerificationLimiterPrefix, limiterLimit, limiterWindowUserID},
+		{&limiter.RefreshUserID, refreshLimiterPrefix, refreshUserIDLimiterLimit, limiterRefreshWindowUserID},
 	}
 
-	if userIDErr := initUserIDLimiter(limiter, client); userIDErr != nil {
-		return nil, userIDErr
+	for _, task := range tasks {
+		instance, err := Limiter(task.prefix, task.limit, task.window, client)
+		if err != nil {
+			return nil, err
+		}
+		*task.target = instance
 	}
+
 	return limiter, nil
-}
-
-func initSessionLimiter(limiter *RateLimiter, client valkey.Client) error {
-	login, loginErr := Limiter(
-		loginLimiterSessionPrefix,
-		limiterLimit,
-		limiterWindowSession,
-		client,
-	)
-	if loginErr != nil {
-		return loginErr
-	}
-
-	loginTwoFactor, loginTwoFactorErr := Limiter(
-		loginTwoFactorSessionLimiterPrefix,
-		limiterLimit,
-		limiterWindowSession,
-		client,
-	)
-	if loginTwoFactorErr != nil {
-		return loginTwoFactorErr
-	}
-
-	forgetPassword, forgetPasswordErr := Limiter(
-		fpLimiterPrefix,
-		limiterLimit,
-		limiterWindowSession,
-		client,
-	)
-	if forgetPasswordErr != nil {
-		return forgetPasswordErr
-	}
-
-	verification, verificationErr := Limiter(
-		verificationSessionLimiterPrefix,
-		limiterLimit,
-		limiterWindowSession,
-		client,
-	)
-	if verificationErr != nil {
-		return verificationErr
-	}
-
-	resetPassword, resetPasswordErr := Limiter(
-		rpSessionLimiterPrefix,
-		limiterLimit,
-		limiterWindowSession,
-		client,
-	)
-	if resetPasswordErr != nil {
-		return resetPasswordErr
-	}
-
-	resendVerification, resendVerificationErr := Limiter(
-		resendVerificationSessionLimiterPrefix,
-		limiterLimit,
-		limiterWindowSession,
-		client,
-	)
-	if resendVerificationErr != nil {
-		return resendVerificationErr
-	}
-
-	accountVerification, accountVerificationErr := Limiter(
-		accountVerificationSessionLimiterPrefix,
-		limiterLimit,
-		limiterWindowSession,
-		client,
-	)
-	if accountVerificationErr != nil {
-		return accountVerificationErr
-	}
-
-	limiter.Login = login
-	limiter.LoginTwoFactor = loginTwoFactor
-	limiter.ForgetPassword = forgetPassword
-	limiter.Verification = verification
-	limiter.ResetPassword = resetPassword
-	limiter.ResendVerification = resendVerification
-	limiter.AccountVerification = accountVerification
-
-	return nil
-}
-
-func initUserIDLimiter(limiter *RateLimiter, client valkey.Client) error {
-	loginTwoFactorUserID, loginTwoFactorUserIDErr := Limiter(
-		loginTwoFactorLimiterPrefix,
-		limiterLimit,
-		limiterWindowUserID,
-		client,
-	)
-	if loginTwoFactorUserIDErr != nil {
-		return loginTwoFactorUserIDErr
-	}
-
-	verificationUserID, verificationUserIDErr := Limiter(
-		verificationLimiterPrefix,
-		limiterLimit,
-		limiterWindowUserID,
-		client,
-	)
-	if verificationUserIDErr != nil {
-		return verificationUserIDErr
-	}
-
-	resetPasswordUserID, resetPasswordUserIDErr := Limiter(
-		rpLimiterPrefix,
-		limiterLimit,
-		limiterWindowUserID,
-		client,
-	)
-	if resetPasswordUserIDErr != nil {
-		return resetPasswordUserIDErr
-	}
-
-	resendVerificationUserID, resendVerificationUserIDErr := Limiter(
-		resendVerificationLimiterPrefix,
-		limiterLimit,
-		limiterWindowUserID,
-		client,
-	)
-	if resendVerificationUserIDErr != nil {
-		return resendVerificationUserIDErr
-	}
-
-	accountVerificationUserID, accountVerificationUserIDErr := Limiter(
-		accountVerificationLimiterPrefix,
-		limiterLimit,
-		limiterWindowUserID,
-		client,
-	)
-	if accountVerificationUserIDErr != nil {
-		return accountVerificationUserIDErr
-	}
-
-	limiter.LoginTwoFactorUserID = loginTwoFactorUserID
-	limiter.VerificationUserID = verificationUserID
-	limiter.ResetPasswordUserID = resetPasswordUserID
-	limiter.ResendVerificationUserID = resendVerificationUserID
-	limiter.AccountVerificationUserID = accountVerificationUserID
-
-	return nil
 }
 
 func Limiter(
